f55d7e4f61fe92a3eaca7120fd5f6957fa0628db
Akhilanand Singh <akhilanand.singh@globant.com>
2019-01-04 08:06:08 +0530

    Jan 4th, 2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article.component';
+import { ArticleService } from './article.service';
+
+//For InMemory testing 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TestData } from './test-data';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      ReactiveFormsModule,
+      HttpClientModule,
+      InMemoryWebApiModule.forRoot(TestData)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+      AppComponent,
+      ArticleComponent
+  ],
+  providers: [
+      ArticleService
+  ],
+  bootstrap: [
+      AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule { } 
